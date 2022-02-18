dee3ee0db87e2fe20e18e80157a23bea20362dac
Sarah Thompson <sthompson@fintechprocessing.com>
2022-02-18 11:52:51 -0500

    began writing if else statements for answer selections

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+//define variables
 const aplha = 'abcdefghijklmnopqrstuvwxyz';
 const num = '1234567890';
 const special = '!@#$%^&*?~_-'
@@ -6,14 +7,45 @@ let len = 0;
 //empty password variable
 let temp = '';
 
-
+//Start function
 function generatePassword () {
+  //Prompt user input
   console.log ("CLICK ACCEPTED!")
   len= prompt('How long would you like your password to be? (8-128)');
+  console.log("Password length" + len);
+
+  if(!len) {
+    alert("Required input")
+  
+  }else if (len <8 || len > 128) {
+  len = prompt("Must choose between 8 - 128 characters");
+  console.log("Password length" + len);
+
+} else {
+
 const caps = confirm('Do you want caps?')
+console.log("Caps" + caps);
 const lower = confirm('Do you want lowercase?')
+console.log("Lower" + lower);
 const spChar = confirm('Do you want special characters?')
+console.log("Special characters" + spChar);
 const numbers = confirm('Do you want numbers?')
+console.log("Numbers" + numbers);
+};
+
+//no answers
+if (!caps && !lower && !spChar && !numbers) {
+  userSelections = alert ("You must select at least one option!");
+
+//4 true values
+} else if (caps && lower && spChar && numbers)
+userSelections = caps.concat(lower, spChar, numbers);
+console.log(userSelections)
+
+
+
+
+
 
   return ("Your password here!")
 }
@@ -33,8 +65,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Start Function
-//Prompt user input
+
 //if no answer
 //4 true choices
 //3 true choices
